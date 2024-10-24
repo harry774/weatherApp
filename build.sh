dc1092e5bd7d4304f549a9c0da7764f729df3cdf
Hardik Patel <xylo.hardik@gmail.com>
2024-10-24 12:01:01 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,3 @@ echo "window.env = {" > ./env.js
 echo "  API_KEY: '$API_KEY'," >> ./env.js
 echo "  API_URL: '$API_URL'" >> ./env.js
 echo "};" >> ./env.js
-
-# Run your normal build process
-npm run build
