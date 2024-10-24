019c41686f1410abf0b99c512a959582a333732f
Hardik Patel <xylo.hardik@gmail.com>
2024-10-24 11:59:14 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # build.sh
 
 # Create a JavaScript file with environment variables
-echo "window.env = {" > /env.js
-echo "  API_KEY: '$API_KEY'," >> /env.js
-echo "  API_URL: '$API_URL'" >> /env.js
-echo "};" >> /env.js
+echo "window.env = {" > ./env.js
+echo "  API_KEY: '$API_KEY'," >> ./env.js
+echo "  API_URL: '$API_URL'" >> ./env.js
+echo "};" >> ./env.js
 
 # Run your normal build process
 npm run build
