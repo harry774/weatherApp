7b8964b27bc2928418fa5345caac5681fd2b002c
Hardik Patel <xylo.hardik@gmail.com>
2024-10-24 11:50:49 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# build.sh
+
+# Create a JavaScript file with environment variables
+echo "window.env = {" > /env.js
+echo "  API_KEY: '$API_KEY'," >> /env.js
+echo "  API_URL: '$API_URL'" >> /env.js
+echo "};" >> /env.js
+
+# Run your normal build process
+npm run build
