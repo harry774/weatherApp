1274048c231443d52d127ecae9f440ccce3c8f89
Hardik Patel <xylo.hardik@gmail.com>
2024-10-24 13:00:05 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 
 # Create a JavaScript file with environment variables
 echo "window.env = {" > ./env.js
-echo "  API_KEY: `$API_KEY`," >> ./env.js
-echo "  API_URL: `$API_URL`" >> ./env.js
+echo "  API_KEY: '${API_KEY}'," >> ./env.js
+echo "  API_URL: '${API_URL}'" >> ./env.js
 echo "};" >> ./env.js
